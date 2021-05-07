7648d2b81c445540679b21f1fc0df8459faa65c2
Taofeek Adegoke <taofeeka@Taofeek-Adegokes-MacBook-Air.local>
2021-05-07 13:33:16 +0100

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Commands.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CommandAPI.Models
+{
+    public class Commands 
+    {
+        [Key]
+        [Required]
+        public int id {get; set;}
+        
+        [Required]
+        [MaxLength(250)]
+        public string HowTo {get; set;}
+
+        [Required]
+        public string Platform {get; set;}
+
+        [Required]
+        public string CommandLine {get; set;}
+
+        
+        public void OnGet()
+        {
+            
+        }
+    }
+}
